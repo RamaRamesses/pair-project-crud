e51c368a852886a3d3509959985af45876ba89f3
Rama Ibrahim <maulanarama@gmail.com>
2020-10-22 12:58:46 +0700

    Added some HTML pages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const mainRoutes = require('./main.js');
 const customerRoutes = require('./customers.js');
 
-
+router.get('/', (req, res) => {
+    res.redirect('/computer-panel')
+})
 router.use('/computer-panel', mainRoutes)
 router.use('/customers', customerRoutes)
 
